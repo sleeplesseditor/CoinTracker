d585da9af7cd0697c108061dd8f12737c84b263b
sleeplesseditor <josh@bryson.me.uk>
2019-01-05 18:19:48 +0000

    Styling update to History component

--- src/History/History.js
+++ src/History/History.js
@@ -110,8 +110,80 @@ class History extends Component {
 
     render() {
         return (
-            <div>
-                History
+            <div className="history--section container">
+                <h2>History (Past 5 Days)</h2>
+                <div className="history--section__box">
+                    <div className="history--section__box__inner">
+                        <h4>{this.state.todayprice.date}</h4>
+                        <div className="columns">
+                            <div className="column">
+                                <p>1 BTC = ${this.state.todayprice.btc}</p>
+                            </div>
+                            <div className="column">
+                                <p>1 ETH = ${this.state.todayprice.eth}</p>
+                            </div>
+                            <div className="column">
+                                <p>1 LTC = ${this.state.todayprice.ltc}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="history--section__box__inner">
+                        <h4>{this.state.yesterdayprice.date}</h4>
+                        <div className="columns">
+                            <div className="column">
+                                <p>1 BTC = ${this.state.yesterdayprice.btc}</p>
+                            </div>
+                            <div className="column">
+                                <p>1 ETH = ${this.state.yesterdayprice.eth}</p>
+                            </div>
+                            <div className="column">
+                                <p>1 LTC = ${this.state.yesterdayprice.ltc}</p>
+                            </div>                        
+                        </div>
+                    </div>
+                    <div className="history--section__box__inner">
+                        <h4>{this.state.twodaysprice.date}</h4>
+                        <div className="columns">
+                            <div className="column">
+                                <p>1 BTC = ${this.state.twodaysprice.btc}</p>
+                            </div>
+                            <div className="column">
+                                <p>1 ETH = ${this.state.twodaysprice.eth}</p>
+                            </div>
+                            <div className="column">
+                                <p>1 LTC = ${this.state.twodaysprice.ltc}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="history--section__box__inner">
+                        <h4>{this.state.threedaysprice.date}</h4>
+                        <div className="columns">
+                            <div className="column">
+                                <p>1 BTC = ${this.state.threedaysprice.btc}</p>
+                            </div>
+                            <div className="column">
+                                <p>1 ETH = ${this.state.threedaysprice.eth}</p>
+                            </div>
+                            <div className="column">
+                                <p>1 LTC = ${this.state.threedaysprice.ltc}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="history--section__box__inner">
+                        <h4>{this.state.fourdaysprice.date}</h4>
+                        <div className="columns">
+                            <div className="column">
+                                <p>1 BTC = ${this.state.fourdaysprice.btc}</p>
+                            </div>
+                            <div className="column">
+                                <p>1 ETH = ${this.state.fourdaysprice.eth}</p>
+                            </div>
+                            <div className="column">
+                                <p>1 LTC = ${this.state.fourdaysprice.ltc}</p>
+                            </div>                        
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
